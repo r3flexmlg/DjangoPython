16d4e9b44cc1d18f4bea4597386cc0abc86c61ad
R3flex <r3flexmlg@gmail.com>
2020-10-09 10:23:11 +0000

    2

---
+++ projectv1/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Projectv1Config(AppConfig):
+    name = 'projectv1'
---
+++ projectv1/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+# Create your views here.
+from django.http import HttpResponse
+
+from .models import Question
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
